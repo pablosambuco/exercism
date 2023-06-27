70084f6359b4f06bbdadea8e3808e378e3ae4951
Pablo <pablosambuco@gmail.com>
2023-06-27 11:23:55 -0300

    varios

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -13,8 +13,8 @@ LITTLE_STRAIGHT = 9
 BIG_STRAIGHT = 10
 CHOICE = 11
 
-
 def score(dice, category):
+    dice.sort()
     d1 = dice[0]
     d2 = dice[1]
     d3 = dice[2]
@@ -22,21 +22,15 @@ def score(dice, category):
     d5 = dice[4]
 
     if category in (ONES, TWOS, THREES, FOURS, FIVES, SIXES):
-        return (
-            category if d1 == category else 0
-            + category if d2 == category else 0
-            + category if d3 == category else 0
-            + category if d4 == category else 0
-            + category if d5 == category else 0
-        )
+        return category*dice.count(category)
     if category == FULL_HOUSE:
-        return 0
+        return sum(dice) if dice.count(d1) == 3 and dice.count(d5) == 2 or dice.count(d1) == 2 and dice.count(d5) == 3 else 0
     if category == FOUR_OF_A_KIND:
-        return 0
+        return 4 * d3 if d1 == d2 == d3 == d4 or d2 == d3 == d4 == d5 else 0
     if category == LITTLE_STRAIGHT:
-        return 30 if dice.sort() == [1,2,3,4,5] else 0
+        return 30 if dice == [1,2,3,4,5] else 0
     if category == BIG_STRAIGHT:
-        return 30 if dice.sort() == [2,3,4,5,6] else 0
+        return 30 if dice == [2,3,4,5,6] else 0
     if category == CHOICE:
         return sum(dice)
     if category == YACHT:
