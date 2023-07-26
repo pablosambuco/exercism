2ab364a2536c5f16ac2292e4341a7807a0eba7bb
Pablo <pablosambuco@gmail.com>
2023-07-26 18:43:49 -0300

    isbn verif

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,17 @@
 def is_valid(isbn):
-    pass
+    clean = isbn.replace("-", "")
+    if (
+        len(clean) != 10
+        or not clean[:-1].isnumeric()
+        or not clean[-1].isnumeric()
+        and clean[-1] != "X"
+    ):
+        return False
+    verif = 0
+    for p, v in enumerate(isbn.replace("-", "")[::-1]):
+        if v == "X":
+            v = 10
+        verif += (p + 1) * int(v)
+        print((p + 1), int(v), (p + 1) * int(v), verif, verif % 11)
+
+    return not verif % 11
