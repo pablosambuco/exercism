20090685936aca4955b6be925af8f949be53c50f
Pablo <pablosambuco@gmail.com>
2023-07-26 18:10:10 -0300

    base convertio

--- python/all-your-base/all_your_base.py
+++ python/all-your-base/all_your_base.py
@@ -1,2 +1,21 @@
 def rebase(input_base, digits, output_base):
-    pass
+    if input_base < 2:
+        raise ValueError("input base must be >= 2")
+
+    if output_base < 2:
+        raise ValueError("output base must be >= 2")
+
+    for c in digits:
+        if c < 0 or c >= input_base:
+            raise ValueError("all digits must satisfy 0 <= d < input base")
+
+    value = sum(input_base**p * v for p, v in enumerate(digits[::-1]))
+    ret = []
+    while value:
+        digit = value % output_base
+        ret.append(digit)
+        value = (value - digit) / output_base
+    if not ret:
+        ret = [0]
+
+    return ret[::-1]
