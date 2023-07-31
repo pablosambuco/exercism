9f3adb8ef10843ddc61e3d8cb7a4a2ae89c6f278
Pablo <pablosambuco@gmail.com>
2023-07-31 19:09:01 -0300

    2 resueltos

--- python/pascals-triangle/pascals_triangle.py
+++ python/pascals-triangle/pascals_triangle.py
@@ -1,2 +1,26 @@
 def rows(row_count):
-    pass
+    if row_count < 0:
+        raise ValueError("number of rows is negative")
+
+    if row_count == 0:
+        return []
+
+    if row_count == 1:
+        return [[1]]
+
+    salida = rows(row_count - 1)
+    nivel_anterior = salida[row_count - 2]
+    new_aux = []
+    vueltas = row_count // 2
+    extra = row_count % 2
+
+    num1 = 0
+    for i in range(vueltas+extra):
+        num2 = nivel_anterior[i]
+        new_aux.append(num1+num2)
+        num1 = num2
+
+    for i in range(vueltas):
+        new_aux.append(new_aux[vueltas-1-i])
+    salida.append(new_aux)
+    return salida
--- python/resistor-color/resistor_color.py
+++ python/resistor-color/resistor_color.py
@@ -1,6 +1,20 @@
+col = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9,
+}
+
+
 def color_code(color):
-    pass
+    return col.get(color)
 
 
 def colors():
-    pass
+    return list(col.keys())
