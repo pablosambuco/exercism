e6fcbac4061395dfd06580318561c17ce514d030
Pablo <pablosambuco@gmail.com>
2023-08-16 18:31:50 -0300

    bs

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -1,2 +1,13 @@
 def find(search_list, value):
-    pass
+    start = 0
+    end = len(search_list) - 1
+    while start <= end:
+        middle = (start + end) // 2
+        if search_list[middle] == value:
+            return middle
+        elif search_list[middle] > value:
+            end = middle - 1
+        else:
+            start = middle + 1
+
+    raise ValueError("value not in array")
