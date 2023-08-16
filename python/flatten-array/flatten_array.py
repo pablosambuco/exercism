bd1b353678495ee6e8320728ea0020e799244a50
Pablo <pablosambuco@gmail.com>
2023-08-16 11:25:46 -0300

    flat

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,2 +1,6 @@
-def flatten(iterable):
-    pass
+def flatten(iterable: list):
+    flat = []
+    for item in iterable:
+        data = flatten(item) if hasattr(item, "__iter__") else [item]
+        flat.extend(x for x in data if x is not None)
+    return flat
