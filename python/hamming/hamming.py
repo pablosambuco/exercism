90f750e920eb179ce66ca0648ac44b87cf1c4a6c
Pablo <pablosambuco@gmail.com>
2023-08-16 11:10:34 -0300

    varios

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,5 @@
 def distance(strand_a, strand_b):
-    pass
+    largo = len(strand_a)
+    if len(strand_b) != largo:
+        raise ValueError("Strands must be of equal length.")
+    return sum(strand_a[i] != strand_b[i] for i in range(largo))
--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,10 @@
+translator = {
+    "G": "C",
+    "C": "G",
+    "T": "A",
+    "A": "U",
+}
+
 def to_rna(dna_strand):
-    pass
+    rna = [translator[x] for x in dna_strand]
+    return "".join(rna)
