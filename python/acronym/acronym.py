625c89f8f97ccce5a42f4697a0ef7508584c0717
Pablo <pablosambuco@gmail.com>
2023-08-17 18:05:27 -0300

    xx

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,13 @@
+def valid(char):
+    return char.isalpha() or char == "'"
+
+
 def abbreviate(words):
-    pass
+    acronym = ""
+    prev_is_not_valid = True
+    for char in words:
+        char_is_valid = valid(char)
+        if char_is_valid and prev_is_not_valid:
+            acronym += char
+        prev_is_not_valid = not char_is_valid
+    return acronym.upper()
--- python/square-root/square_root.py
+++ python/square-root/square_root.py
@@ -1,2 +1,10 @@
-def square_root(number):
-    pass
+"""Newtons approximation to square root"""
+
+
+def square_root(n):
+    px = n  # Initial guess
+    while True:
+        x = 0.5 * (px + n / px)
+        if abs(px - x) < 1:
+            return int(x)  # best approximation
+        px = x
