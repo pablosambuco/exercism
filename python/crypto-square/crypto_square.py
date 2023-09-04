2226e2b877d3e77d5a151f8cfb2d2f3e1636bda4
Pablo <pablosambuco@gmail.com>
2023-09-04 18:49:00 -0300

    varios

--- python/crypto-square/crypto_square.py
+++ python/crypto-square/crypto_square.py
@@ -1,2 +1,18 @@
 def cipher_text(plain_text):
-    pass
+    normalized = "".join([char for char in plain_text if char.isalnum()]).lower()
+    length = len(normalized)
+    if not length:
+        return ""
+    rows = cols = int(length ** (0.5))
+    if rows * cols < length:
+        cols += 1
+    if rows * cols < length:
+        rows += 1
+    padding = rows * cols - length
+    normalized += " " * padding
+    # transposing columns and rows for everything thar follows
+    out = [[[] for _ in range(rows)] for _ in range(cols)]
+    for pos, char in enumerate(normalized):
+        c, r = divmod(pos, cols)
+        out[r][c] = char
+    return " ".join("".join(row) for row in out)
--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -1,6 +1,28 @@
+"""RLE"""
+
+
 def decode(string):
-    pass
+    out = []
+    number = 0
+    for char in string:
+        if char.isnumeric():
+            number = number * 10 + int(char)
+        else:
+            out.extend([char] * (number or 1))
+            number = 0
+    return "".join(out)
 
 
 def encode(string):
-    pass
+    if not string:
+        return ""
+    out = []
+    count = 1
+    for i in range(1, len(string)):
+        if string[i] == string[i - 1]:
+            count += 1
+        else:
+            out.extend((str(count) if count > 1 else "", string[i - 1]))
+            count = 1
+    out.extend((str(count) if count > 1 else "", string[-1]))
+    return "".join(out)
