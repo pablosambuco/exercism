5fd40c874af7ecdcc048ded9d6aec19a29cd67af
Pablo <pablosambuco@gmail.com>
2023-09-08 17:33:41 -0300

    change, gpt all the way

--- python/change/change.py
+++ python/change/change.py
@@ -1,2 +1,26 @@
+"""Fewest coins
+Not proud of the algorithm, it was gtp all the way...
+"""
+
+
 def find_fewest_coins(coins, target):
-    pass
+    if target < 0:
+        raise ValueError("target can't be negative")
+
+    minimal_coins = {0: []}
+
+    for t in range(1, target + 1):
+        minimal_coins[t] = None
+        for coin in coins:
+            if t - coin >= 0 and minimal_coins[t - coin] is not None:
+                current_combination = minimal_coins[t - coin] + [coin]
+                if minimal_coins[t] is None or len(current_combination) < len(
+                    minimal_coins[t]
+                ):
+                    minimal_coins[t] = current_combination
+
+    minimal = minimal_coins[target]
+    if minimal is None:
+        raise ValueError("can't make target with given coins")
+
+    return sorted(minimal)
