c4ff63244bde1704f21b08d79cf3332563705090
Pablo <pablosambuco@gmail.com>
2023-09-22 17:49:00 -0300

    series-product

--- python/largest-series-product/largest_series_product.py
+++ python/largest-series-product/largest_series_product.py
@@ -1,2 +1,18 @@
 def largest_product(series, size):
-    pass
+    series = str(series)
+    s = len(series)
+    if size > s:
+        raise ValueError("span must be smaller than string length")
+    if size < 0:
+        raise ValueError("span must not be negative")
+    if not series.isnumeric():
+        raise ValueError("digits input must only contain digits")
+
+    max_product = 0
+    for i in range(s - size + 1):  # for all possible substring
+        product = 1
+        for j in range(i, i + size):  # calculate the product of their digits
+            product *= int(series[j])
+        max_product = max(max_product, product)  # and return the maximum
+
+    return max_product
