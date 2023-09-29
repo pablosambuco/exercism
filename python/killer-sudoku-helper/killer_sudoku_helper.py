472e3ac9511734fa07918f9d5ad979941100cec8
Pablo <pablosambuco@gmail.com>
2023-09-29 16:56:55 -0300

    sudoku

--- python/killer-sudoku-helper/killer_sudoku_helper.py
+++ python/killer-sudoku-helper/killer_sudoku_helper.py
@@ -1,2 +1,14 @@
+"""killer sudoku helper"""
+import itertools
+
+
 def combinations(target, size, exclude):
-    pass
+    numbers = [num for num in range(1, 10) if num <= target and num not in exclude]
+    if not numbers:
+        return []
+
+    return [
+        list(comb)
+        for comb in itertools.combinations(numbers, size)
+        if sum(comb) == target
+    ]
