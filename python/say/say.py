819d84a5d85623e92192f88010e9d48545d87eda
Pablo <pablosambuco@gmail.com>
2023-10-11 15:12:01 -0300

    say

--- python/say/say.py
+++ python/say/say.py
@@ -1,13 +1,15 @@
 """say"""
 
-scale = {
+thousands = {
     1000000000: "billion",
     1000000: "million",
     1000: "thousand",
     100: "hundred",
-    1: ""
+    1: "",
 }
-quantities = {
+
+numbers = {
+    100: "hundred",
     90: "ninety",
     80: "eighty",
     70: "seventy",
@@ -37,22 +39,44 @@ quantities = {
     1: "one",
 }
 
+
+def factorice(number, factors):
+    number_dict = {}
+    for factor in factors:
+        value, number = divmod(number, factor)
+        if value:
+            number_dict[factor] = value
+    return number_dict
+
+
 def say(number):
     if not 0 <= number <= 999999999999:
         raise ValueError("input out of range")
+
     if number == 0:
         return "zero"
-    ss = []
-    for s, st in scale.items():
-        if number >= s:
-            sn = number // s
-            sq = []
-            for q, qt in quantities.items():
-                if sn >= q:
-                    sn %= q
-                    sq.append(qt)
-            number %= s
-            ss.append("-".join(sq))
-            if st:
-                ss.append(st)
-    return " ".join(ss)
+
+    big_scale = factorice(number, thousands)
+    for number in big_scale:
+        big_scale[number] = factorice(big_scale[number], numbers)
+
+    number_list = []
+    sep = " "
+    while big_scale:
+        scale, value = big_scale.popitem()
+        number_list.insert(0, sep)
+        number_list.insert(0, thousands[scale])
+        previous = 999
+        while value:
+            small_scale, small_value = value.popitem()
+            separator = " "
+            if previous < 100 and small_scale < 100:
+                separator = "-"
+            number_list.insert(0, separator)
+            number_list.insert(0, numbers[small_scale])
+            if small_value > 1:
+                number_list.insert(0, separator)
+                number_list.insert(0, numbers[small_value])
+            previous = small_scale
+
+    return "".join(number_list).strip()
