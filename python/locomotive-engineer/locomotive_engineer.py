a5457e7428f9a99e0ca6100d331e735e2895ea46
Pablo <pablosambuco@gmail.com>
2023-10-12 12:30:28 -0300

    loco

--- python/locomotive-engineer/locomotive_engineer.py
+++ python/locomotive-engineer/locomotive_engineer.py
@@ -1,13 +1,15 @@
 """Functions which helps the locomotive engineer to keep track of the train."""
 
 
-def get_list_of_wagons():
+def get_list_of_wagons(*args):
     """Return a list of wagons.
 
     :param: arbitrary number of wagons.
     :return: list - list of wagons.
     """
-    pass
+    (*lista,) = args
+    print(lista)
+    return lista
 
 
 def fix_list_of_wagons(each_wagons_id, missing_wagons):
@@ -17,17 +19,27 @@ def fix_list_of_wagons(each_wagons_id, missing_wagons):
     :parm missing_wagons: list - the list of missing wagons.
     :return: list - list of wagons.
     """
-    pass
+    [first, second, *rest] = each_wagons_id
+    (*each_wagons_id,) = *rest, *[first, second]
+    uno = each_wagons_id.index(1)
+    (*wagons,) = (
+        *each_wagons_id[0 : uno + 1],
+        *missing_wagons,
+        *each_wagons_id[uno + 1 :],
+    )
+    print(list(wagons))
+    return wagons
 
 
-def add_missing_stops():
+def add_missing_stops(route, **kwargs):
     """Add missing stops to route dict.
 
     :param route: dict - the dict of routing information.
     :param: arbitrary number of stops.
     :return: dict - updated route dictionary.
     """
-    pass
+    stops = list(kwargs.values())
+    return {**route, "stops": stops}
 
 
 def extend_route_information(route, more_route_information):
@@ -37,7 +49,7 @@ def extend_route_information(route, more_route_information):
     :param more_route_information: dict -  extra route information.
     :return: dict - extended route information.
     """
-    pass
+    return {**route, **more_route_information}
 
 
 def fix_wagon_depot(wagons_rows):
@@ -46,4 +58,5 @@ def fix_wagon_depot(wagons_rows):
     :param wagons_rows: list[list[tuple]] - the list of rows of wagons.
     :return: list[list[tuple]] - list of rows of wagons.
     """
-    pass
+    [[a,b,c],[d,e,f],[g,h,i]] = wagons_rows
+    return [[a,d,g],[b,e,h],[c,f,i]]
