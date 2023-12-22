43e18f854cbc46530dfcefe642672e8be547d0fb
Pablo <pablosambuco@gmail.com>
2023-12-22 15:44:15 -0300

    giga

--- python/OK/spiral-matrix/spiral_matrix.py
+++ python/OK/spiral-matrix/spiral_matrix.py
@@ -7,11 +7,10 @@ def spiral_matrix(size):
     i = j = k = 0
     r, c = next(increment)
 
-    while k < size**2:
+    for k in range(size**2):
         if not (0 <= j + c < size and 0 <= i + r < size and matrix[i + r][j + c] == 0):
             r, c = next(increment)
-        k += +1
-        matrix[i][j] = k
+        matrix[i][j] = k + 1
         i, j = i + r, j + c
 
     return matrix
