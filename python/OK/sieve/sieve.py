881112939dfaad3c05fb70317094b7b99fe48875
Pablo <pablosambuco@gmail.com>
2024-01-02 14:16:25 -0300

    sieve

--- python/OK/sieve/sieve.py
+++ python/OK/sieve/sieve.py
@@ -1,7 +1,7 @@
 def primes(limit):
-    is_prime = {x: True for x in range(2, limit + 1)}
+    is_prime = [x for x in range(limit + 1)]
     for x in range(2, int(limit**0.5) + 1):
         if is_prime[x]:
             for y in range(x * 2, limit + 1, x):
                 is_prime[y] = False
-    return [x for x in is_prime if is_prime[x]]
+    return [x for x in is_prime[2:] if is_prime[x]]
