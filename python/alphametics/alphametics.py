2aa6cf816413146da6812a54a5ec5479c4f150d1
Pablo <pablosambuco@gmail.com>
2024-01-04 18:51:23 -0300

    cosas...

--- python/alphametics/alphametics.py
+++ python/alphametics/alphametics.py
@@ -1,2 +1,18 @@
 def solve(puzzle):
-    pass
+    puzzle = "SEND + MORE == MONEY"
+    words = puzzle.split()
+    words.remove("+")
+    words.remove("==")
+
+    counts = {}
+    translation = {}
+    for word in words:
+        for letter in word:
+            if letter.isalpha():
+                counts[letter] = counts.get(letter, 0) + 1
+
+    if words:
+        if len(words[-1]) > len(words[0]):
+            translation[words[-1][0]] = "1"
+
+    #TODO TERMINAR ESTO!
