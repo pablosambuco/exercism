60b07da9723241170514a739bcb334e0618b2b9a
Pablo Sambuco <pablosambuco@gmail.com>
2024-09-16 17:35:11 -0300

    varios

--- python/phone-number/phone_number.py
+++ python/phone-number/phone_number.py
@@ -1,3 +1,49 @@
 class PhoneNumber:
     def __init__(self, number):
-        pass
+        self.area_code, self.exchange_code, self.suscriber = self.cleanup(number)
+
+    @property
+    def number(self):
+        return self.area_code + self.exchange_code + self.suscriber
+
+    def pretty(self):
+        return f"({self.area_code})-{self.exchange_code}-{self.suscriber}"
+
+    def cleanup(self, number: str):
+        # Remove valid punctuation
+        number = number.translate({ord(i): None for i in "+()-. "})
+
+        # Are ther any invalid characters?
+        if any(c.isalpha() for c in number):
+            raise ValueError("letters not permitted")
+
+        if not all(c.isdigit() for c in number):
+            raise ValueError("punctuations not permitted")
+
+        # Count digits
+        digits = len(number)
+        if digits < 10:
+            raise ValueError("must not be fewer than 10 digits")
+        if digits > 11:
+            raise ValueError("must not be greater than 11 digits")
+
+        # Validate 11 digits number
+        if digits == 11:
+            if not number.startswith("1"):
+                raise ValueError("11 digits must start with 1")
+            number = number[1:]
+
+        area_code = number[0:3]
+        exchange_code = number[3:6]
+        number = number[6:]
+
+        if area_code.startswith("0"):
+            raise ValueError("area code cannot start with zero")
+        if area_code.startswith("1"):
+            raise ValueError("area code cannot start with one")
+        if exchange_code.startswith("0"):
+            raise ValueError("exchange code cannot start with zero")
+        if exchange_code.startswith("1"):
+            raise ValueError("exchange code cannot start with one")
+
+        return area_code, exchange_code, number
--- python/queen-attack/queen_attack.py
+++ python/queen-attack/queen_attack.py
@@ -1,6 +1,26 @@
 class Queen:
     def __init__(self, row, column):
-        pass
+        if row < 0:
+            raise ValueError("row not positive")
+        if row > 7:
+            raise ValueError("row not on board")
+        if column < 0:
+            raise ValueError("column not positive")
+        if column > 7:
+            raise ValueError("column not on board")
 
-    def can_attack(self, another_queen):
-        pass
+        self.row = row
+        self.column = column
+
+    def is_at(self, row, column):
+        return self.row == row and self.column == column
+
+    def can_attack(self, other: "Queen"):
+        if self.row == other.row and self.column == other.column:
+            raise ValueError("Invalid queen position: both queens in the same square")
+
+        return (
+            self.row == other.row
+            or self.column == other.column
+            or abs(self.row - other.row) == abs(self.column - other.column)
+        )
