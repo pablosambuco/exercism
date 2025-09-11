434baaba55c7c60501cb42f206f0dde58de4f229
Pablo Sambuco <pablosambuco@gmail.com>
2025-09-11 18:27:20 -0300

    highscores

---
+++ solutions/python/high-scores/high_scores.py
@@ -0,0 +1,13 @@
+class HighScores:
+    def __init__(self, scores):
+        self.scores = scores
+        self.sorted_scores = sorted(scores, reverse=True)
+
+    def latest(self):
+        return self.scores[-1]
+    
+    def personal_best(self):
+        return self.sorted_scores[0]
+    
+    def personal_top_three(self):
+        return self.sorted_scores[:3]
