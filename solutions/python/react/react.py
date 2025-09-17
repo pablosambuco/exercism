dfc312c942550cdeb883ede593114da4d0284dce
Pablo Sambuco <pablosambuco@gmail.com>
2025-09-17 19:23:05 -0300

    post-gpt

--- solutions/python/react/react.py
+++ solutions/python/react/react.py
@@ -1,5 +1,6 @@
 class InputCell:
     """A cell that holds a value and notifies dependents on change."""
+
     def __init__(self, value):
         self._value = value
         self.callbacks = []
@@ -10,82 +11,36 @@ class InputCell:
         """Get the current value"""
         return self._value
 
-    def set_value(self, value):
-        """Set the value without propagation"""
-        self._value = value
-
     @value.setter
     def value(self, value):
-        """Set the value and propagate changes"""
         if self._value != value:
             self._value = value
+            changed = {}
+            for dep in self.dependents:
+                dep.recompute(changed)
+            for cell, original_value in changed.items():
+                if cell.value != original_value:
+                    for cb in cell.callbacks:
+                        cb(cell.value)
 
-            affected = []
-            queue = list(self.dependents)
-            for cell in queue:
-                if cell not in affected:
-                    affected.append(cell)
-                    if hasattr(cell, "dependents"):
-                        queue.extend(cell.dependents)
 
-            ordered = []
-            visited = set()
+class ComputeCell(InputCell):
+    """A cell whose value is computed from other cells."""
 
-            def visit(cell):
-                if cell in visited:
-                    return
-                visited.add(cell)
-                if hasattr(cell, "inputs"):
-                    for inp in cell.inputs:
-                        if inp in affected:
-                            visit(inp)
-                ordered.append(cell)
+    def __init__(self, inputs, compute_function):
+        super().__init__(compute_function([i.value for i in inputs]))
+        self.inputs = inputs
+        self.compute_function = compute_function
+        for i in inputs:
+            i.dependents.add(self)
 
-            for cell in affected:
-                visit(cell)
-
-            changed = set()
-            for cell in ordered:
-                if hasattr(cell, "compute_function"):
-                    old = cell.value
-                    new = cell.compute_function(cell.inputs)
-                    if old != new:
-                        cell.set_value(new)
-                        changed.add(cell)
-
-            for cell in changed:
-                for cb in cell.callbacks:
-                    cb(cell.value)
-
-    def __hash__(self):
-        return id(self)
-
-    def __repr__(self):
-        return f"({self._value},{len(self.callbacks)})"
-
-    def __add__(self, other):
-        return self._value + other
-
-    def __radd__(self, other):
-        return self._value + other
-
-    def __sub__(self, other):
-        return self._value - other
-
-    def __rsub__(self, other):
-        return other - self._value
-
-    def __mul__(self, other):
-        return self._value * other
-
-    def __rmul__(self, other):
-        return self._value * other
-
-    def __lt__(self, other):
-        return self._value < other
-
-    def __eq__(self, other):
-        return self._value == other
+    def recompute(self, changed={}):
+        new = self.compute_function([i.value for i in self.inputs])
+        if new != self._value:
+            changed.setdefault(self, self._value)
+            self._value = new
+            for dep in self.dependents:
+                dep.recompute(changed)
 
     def add_callback(self, callback):
         """adds a callback object to the list"""
@@ -95,14 +50,3 @@ class InputCell:
         """removes a callback object from the list"""
         if callback in self.callbacks:
             self.callbacks.remove(callback)
-
-
-class ComputeCell(InputCell):
-    """A cell whose value is computed from other cells."""    
-    def __init__(self, inputs, compute_function):
-        self.compute_function = compute_function
-        self.inputs = inputs
-        super().__init__(compute_function(inputs))
-        for i in inputs:
-            if hasattr(i,'dependents'):
-                i.dependents.add(self)
